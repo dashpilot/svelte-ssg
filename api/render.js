f0c85d21e4b5f341754f82b0d1a71936891845ff
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-08-24 00:53:42 +0200

    added api folder

---
+++ api/render.js
@@ -0,0 +1,22 @@
+
+import template from "./../modules/index.mjs";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async (request) => {
+    const data = {
+        "posts": [
+          {
+            "page": "index",
+            "title": "Welcome to my site",
+            "body": "Lorem ipsum dolor sit amet"
+          }]
+        }
+  const { default: Component } = await import(`./precompiled/pages/index.js`);
+  const result = Component.render({ data: data });
+  return new Response(result, {
+    headers: { "content-type": "text/html" },
+  });
+};
