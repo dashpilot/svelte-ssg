2691b2a770fb8864503715b77c03187ca9b082cd
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-08-24 01:40:09 +0200

    tweaks

--- api/render.js
+++ api/render.js
@@ -6,13 +6,24 @@ export default async (request) => {
 
   console.log(request.url);
 
+
+  const { searchParams } = new URL(req.url);
+  var page = searchParams.get("page");
+  page.replace('.html', '');
+
+  console.log(page)
+
+  if(page==''){
+    page = 'index';
+  }
+
   const resp = await fetch("https://svelte-ssg.vercel.app/data.json");
 	const data = await resp.json();
 
   const resp2 = await fetch("https://svelte-ssg.vercel.app/_layout.html");
 	const layout = await resp2.text();
 
-  const { default: Component } = await import(`./../precompiled/pages/index.js`);
+  const { default: Component } = await import(`./../precompiled/pages/${page}.js`);
   const result = Component.render({ data: data });
 
   const body = layout.replace('{body}', result.html);
