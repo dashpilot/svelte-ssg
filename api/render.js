84a8ecf6c0f16a57eeb757976c6b0cdf0188cfd6
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-08-24 01:51:11 +0200

    reverting

--- api/render.js
+++ api/render.js
@@ -20,7 +20,7 @@ export default async (request) => {
   const resp2 = await fetch("https://svelte-ssg.vercel.app/_layout.html");
 	const layout = await resp2.text();
 
-  const { default: Component } = await import(`./../precompiled/pages/${page}.js`);
+  const { default: Component } = await import(`./../precompiled/pages/index.js`);
   const result = Component.render({ data: data });
 
   const body = layout.replace('{body}', result.html);
